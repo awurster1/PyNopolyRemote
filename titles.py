aecde9c45809cf51180e80431d03daa0164322ff
DESKTOP-4PSMA19\awurs <awurster1@gmail.com>
2017-11-08 17:54:41 -0600

    v1.2 Tab errors fixed. Dice order function moved above the main menu function.

--- titles.py
+++ titles.py
@@ -1,36 +1,38 @@
-go = {'name' = 'go', 'land_effect' = '200', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-ivarstead = {'name' = 'ivarstead', 'color' = 'purple' 'land_effect' = '60', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-riften = {'name' = 'riften', 'color' = 'purple' 'land_effect' = '60', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-riften = {'name' = 'riften', 'color' = 'purple' 'land_effect' = '60', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-imperial_tax = {'name' = 'imperial_tax', 'color' = None, 'land_effect' = '200', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-moorside_inn = {'name' = 'moorside_inn', 'color' = None, 'land_effect' = '200', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-rorikstead = {'name' = 'rorikstead', 'color' = 'sky_blue', 'land_effect' = '100', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-riverwood = {'name' = 'riverwood', 'color' = 'sky_blue', 'land_effect' = '100', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-whiterun = {'name' = 'whiterun', 'color' = 'sky_blue', 'land_effect' = '120', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-jail = {'name' = 'jail', 'color' = None, 'land_effect' = '50', 'house1' = None, 'house2' = None, 'house3' = None, 'hotel' = None}
-jail_visit = {'name' = 'jail_visit', 'color' = None, 'land_effect' = None, 'house1' = None, 'house2' = None, 'house3' = None, 'hotel' = None}
-meekos_shack = {'name' = 'meekos_shack', 'color' = 'pink', 'land_effect' = '140', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-markarth_tressury = {'name' = 'markarth_tressury', 'color' = 'lightbulb', 'land_effect' = '150', 'house1' = None, 'house2' = None, 'house3' = None, 'hotel' = None}
-stone_hills = {'name' = 'stone_hills', 'color' = 'pink', 'land_effect' = '140', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-morthal = {'name' = 'morthal', 'color' = 'pink', 'land_effect' = '160', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-nightgale_inn = {'name' = 'nightgale_inn', 'color' = None, 'land_effect' = '200', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-loreius_farm = {'name' = 'loreius_farm', 'color' = 'orange', 'land_effect' = '180', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-angas_mill = {'name' = 'angas_mill', 'color' = 'orange', 'land_effect' = '180', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-dawnstar = {'name' = 'dawnstar', 'color' = 'orange', 'land_effect' = '200', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-free_parking = {'name' = 'free_parking', 'color' = None, 'land_effect' = None, 'house1' = None, 'house2' = None, 'house3' = None, 'hotel' = None}
-cliffside_retreat = {'name' = 'cliffside_retreat', 'color' = 'red', 'land_effect' = '220', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-mor_khazgor = {'name' = 'mor_khazgor', 'color' = 'red', 'land_effect' = '220', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-markarth = {'name' = 'markarth', 'color' = 'red', 'land_effect' = '240', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-windpeak_inn = {'name' = 'windpeak_inn', 'color' = None, 'land_effect' = '200', 'house1' = None, 'house2' = None, 'house3' = None, 'hotel' = None}
-japhets_folly = {'name' = 'japhets_folly', 'color' = 'yellow', 'land_effect' = '260', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-the_chill = {'name' = 'the_chill', 'color' = 'yellow', 'land_effect' = '260', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-forge = {'name' = 'forge', 'color' = 'faucet', 'land_effect' = '150', 'house1' = None, 'house2' = None, 'house3' = None, 'hotel' = None}
-winterhold = {'name' = 'winterhold', 'color' = 'yellow', 'land_effect' = '280', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-gotojail = {'name' = 'go_to_jail', 'color' = None, 'land_effect' = None, 'house1' = None, 'house2' = None, 'house3' = None, 'hotel' = None}
-kynesgrove = {'name' = 'kynesgrove', 'color' = 'green', 'land_effect' = '300', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-darkwater_crossing = {'name' = 'darkwater_crossing', 'color' = 'green', 'land_effect' = '300', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-windhelm = {'name' = 'windhelm', 'color' = 'green', 'land_effect' = '320', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-vilemy_inn = {'name' = 'vilemy_inn', 'color' = None, 'land_effect' = '200', 'house1' = None, 'house2' = None, 'house3' = None, 'hotel' = None}
-dragon_bridge = {'name' = 'dragon_bridge', 'color' = 'dark_blue', 'land_effect' = '350', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
-aldmeri_tax = {'name' = 'aldmeri_tax', 'color' = None, 'land_effect' = '75', 'house1' = None, 'house2' = None, 'house3' = None, 'hotel' = None}
-solitude = {'name' = 'solitude', 'color' = 'dark_blue', 'land_effect' = '400', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
+go = {'name': 'go', 'land_effect': '200', 'house1': ' ', 'house2': ' ', 'house3': ' ', 'hotel': ' '}
+ivarstead = {'name': 'ivarstead', 'color': 'purple', 'land_effect': '60', 'house1': ' ', 'house2': ' ', 'house3': ' ', 'hotel': ' '}
+riften = {'name': 'riften', 'color': 'purple', 'land_effect': '60', 'house1': ' ', 'house2': ' ', 'house3': ' ', 'hotel': ' '}
+imperial_tax = {'name': 'imperial_tax', 'color': None, 'land_effect': '200', 'house1': ' ', 'house2': ' ', 'house3': ' ', 'hotel': ' '}
+moorside_inn = {'name': 'moorside_inn', 'color': None, 'land_effect': '200', 'house1': ' ', 'house2':  ' ', 'house3': ' ', 'hotel': ' '}
+rorikstead = {'name': 'rorikstead', 'color': 'sky_blue', 'land_effect': '100', 'house1': ' ', 'house2': ' ', 'house3': ' ', 'hotel': ' '}
+riverwood = {'name': 'riverwood', 'color': 'sky_blue', 'land_effect': '100', 'house1': ' ', 'house2': ' ', 'house3': ' ', 'hotel': ' '}
+whiterun = {'name': 'whiterun', 'color': 'sky_blue', 'land_effect': '120', 'house1': ' ', 'house2': ' ', 'house3': ' ', 'hotel': ' '}
+jail = {'name': 'jail', 'color': None, 'land_effect': '50', 'house1': None, 'house2': None, 'house3': None, 'hotel': None}
+jail_visit = {'name': 'jail_visit', 'color': None, 'land_effect': None, 'house1': None, 'house2': None, 'house3': None, 'hotel': None}
+
+#commenting out incomplete code
+
+# meekos_shack = {'name': 'meekos_shack', 'color': 'pink', 'land_effect' = '140', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel': ' '}
+# markarth_tressury = {'name': 'markarth_tressury', 'color': 'lightbulb', 'land_effect': '150', 'house1': None, 'house2': None, 'house3': None, 'hotel': None}
+# stone_hills = {'name': 'stone_hills', 'color': 'pink', 'land_effect': '140', 'house1': ' ', 'house2': ' ', 'house3': ' ', 'hotel': ' '}
+# morthal = {'name': 'morthal', 'color': 'pink', 'land_effect': '160', 'house1': ' ', 'house2': ' ', 'house3': ' ', 'hotel': ' '}
+# nightgale_inn = {'name': 'nightgale_inn', 'color': None, 'land_effect': '200', 'house1': ' ', 'house2': ' ', 'house3': ' ', 'hotel': ' '}
+# loreius_farm = {'name': 'loreius_farm', 'color': 'orange', 'land_effect': '180', 'house1': ' ', 'house2': ' ', 'house3': ' ', 'hotel': ' '}
+# angas_mill = {'name': 'angas_mill', 'color': 'orange', 'land_effect': '180', 'house1': ' ', 'house2': ' ', 'house3': ' ', 'hotel': ' '}
+# dawnstar = {'name': 'dawnstar', 'color': 'orange', 'land_effect': '200', 'house1': ' ', 'house2': ' ', 'house3': ' ', 'hotel': ' '}
+# free_parking = {'name': 'free_parking', 'color': None, 'land_effect': None, 'house1': None, 'house2': None, 'house3': None, 'hotel': None}
+# cliffside_retreat = {'name': 'cliffside_retreat', 'color': 'red', 'land_effect': '220', 'house1': ' ', 'house2': ' ', 'house3': ' ', 'hotel': ' '}
+# mor_khazgor = {'name': 'mor_khazgor', 'color': 'red', 'land_effect': '220', 'house1': ' ', 'house2': ' ', 'house3': ' ', 'hotel': ' '}
+# markarth = {'name': 'markarth', 'color': 'red', 'land_effect' = '240', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
+# windpeak_inn = {'name': 'windpeak_inn', 'color': None, 'land_effect' = '200', 'house1' = None, 'house2' = None, 'house3' = None, 'hotel' = None}
+# japhets_folly = {'name': 'japhets_folly', 'color': 'yellow', 'land_effect' = '260', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
+# the_chill = {'name': 'the_chill', 'color': 'yellow', 'land_effect' = '260', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
+# forge = {'name': 'forge', 'color': 'faucet', 'land_effect' = '150', 'house1' = None, 'house2' = None, 'house3' = None, 'hotel' = None}
+# winterhold = {'name': 'winterhold', 'color': 'yellow', 'land_effect' = '280', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
+# gotojail = {'name': 'go_to_jail', 'color': None, 'land_effect' = None, 'house1' = None, 'house2' = None, 'house3' = None, 'hotel' = None}
+# kynesgrove = {'name': 'kynesgrove', 'color': 'green', 'land_effect' = '300', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
+# darkwater_crossing = {'name': 'darkwater_crossing', 'color': 'green', 'land_effect' = '300', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
+# windhelm = {'name': 'windhelm', 'color': 'green', 'land_effect' = '320', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
+# vilemy_inn = {'name': 'vilemy_inn', 'color': None, 'land_effect' = '200', 'house1' = None, 'house2' = None, 'house3' = None, 'hotel' = None}
+# dragon_bridge = {'name': 'dragon_bridge', 'color': 'dark_blue', 'land_effect' = '350', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
+# aldmeri_tax = {'name': 'aldmeri_tax', 'color': None, 'land_effect' = '75', 'house1' = None, 'house2' = None, 'house3' = None, 'hotel' = None}
+# solitude = {'name': 'solitude', 'color': 'dark_blue', 'land_effect' = '400', 'house1' = ' ', 'house2' = ' ', 'house3' = ' ', 'hotel' = ' '}
